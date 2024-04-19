1366f9c37561b3380577bd2d1df881a42d92fdbb
Afif <juventinopel@gmail.com>
2024-04-20 00:45:22 +0700

    Add CSV export functionality to print_div function in main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 def print_div(soup):
-    for div in soup.find_all('div', class_='col-md-3 card-menu'):
-        item_name = div.find('h5').text.strip()
-        item_description = div.find('p').text.strip()
-
-        print(f'Item Name: {item_name}')
-        print(f'Item Description: {item_description}')
-        print('------------------------')
+    with open('/Users/afiffauzi/Documents/Code/beautiful-remboelan/menu.csv', 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for div in soup.find_all('div', class_='col-md-3 card-menu'):
+            item_name = div.find('h5').text.strip()
+            item_description = div.find('p').text.strip()
+            writer.writerow([item_name, item_description])
 
 for i in range(1, 14):
     response = requests.get(f'https://remboelan.com/menu/{i}')
