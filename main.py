60c75bb2d1ac3a1f441c993dbb5a7733ed15663b
Afif <juventinopel@gmail.com>
2024-04-20 00:49:33 +0700

    Edit print_div function to include dynamic file path in main.py

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+import os
 
 def print_div(soup):
-    with open('/Users/afiffauzi/Documents/Code/beautiful-remboelan/menu.csv', 'a', newline='') as csvfile:
+    base_dir = '/Users/****/Documents/Code/****'
+    output_file = os.path.join(base_dir, 'menu.csv')
+
+    with open(output_file, 'a', newline='') as csvfile:
         writer = csv.writer(csvfile)
         for div in soup.find_all('div', class_='col-md-3 card-menu'):
             item_name = div.find('h5').text.strip()
