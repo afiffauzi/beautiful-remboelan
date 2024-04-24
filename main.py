b11015a6c8b43bc12dc1ea75d573a3ca79d8b39d
Afif <juventinopel@gmail.com>
2024-04-24 16:55:48 +0700

    Add img_url and readability

--- main.py
+++ main.py
@@ -3,20 +3,24 @@ import requests
 import csv
 import os
 
-def print_div(soup):
+# Function to extract data from parsed HTML and write to CSV
+def extract_data_and_write_to_csv(parsed_html):
     base_dir = '/Users/****/Documents/Code/****'
-    output_file = os.path.join(base_dir, 'menu.csv')
+    output_csv_file_path = os.path.join(base_dir, 'menu.csv')
 
-    with open(output_file, 'a', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        for div in soup.find_all('div', class_='col-md-3 card-menu'):
+    with open(output_csv_file_path, 'a', newline='') as output_csv_file:
+        writer = csv.writer(output_csv_file)
+        for div in parsed_html.find_all('div', class_='col-md-3 card-menu'):
+            # Extract items
             item_name = div.find('h5').text.strip()
             item_description = div.find('p').text.strip()
-            writer.writerow([item_name, item_description])
+            image_url = div.find('img')['src']
+            writer.writerow([item_name, item_description, image_url])
 
+# Loop over each page of the menu. 14 is ofc hardcoded, but you can find a way to get the total number of pages
 for i in range(1, 14):
     response = requests.get(f'https://remboelan.com/menu/{i}')
     soup = BeautifulSoup(response.text, 'html.parser')
-    print_div(soup)
+    extract_data_and_write_to_csv(soup)
 
 
